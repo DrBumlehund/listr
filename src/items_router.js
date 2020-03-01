edbd96cf176e9a6d73ed6fd9c2acde3aa8cbca7f
Thomas Lemqvist <thomas@lemqvist.dk>
2020-03-01 23:34:07 +0100

    added 'X-Accel-Buffering': 'no' header to sse response

--- src/items_router.js
+++ src/items_router.js
@@ -50,7 +50,8 @@ router.get('/events', (req, res, next) => {
     const headers = {
         'Content-Type': 'text/event-stream',
         'Connection': 'keep-alive',
-        'Cache-Control': 'no-cache'
+        'Cache-Control': 'no-cache',
+        'X-Accel-Buffering': 'no'
     };
     res.writeHead(200, headers);
 
